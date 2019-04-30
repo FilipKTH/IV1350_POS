0f813668f32fef3498e76c96a2efaee381c0e755
Filip <filipgar@kth.se>
2019-04-30 16:43:32 +0200

    Change purchase test based on the new way of entering quantity

--- tests/se/kth/iv1350/model/PurchaseTest.java
+++ tests/se/kth/iv1350/model/PurchaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.kth.iv1350.integration.ItemIdentifier;
+import se.kth.iv1350.integration.PurchaseItemDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,18 +21,10 @@ class PurchaseTest {
     void tearDown() {
     }
 
-    /*
-    Things to test:
-    1: adding a existing (database) item
-    2: adding null item
-    3: adding item with bigger quantity than 1
-    4: adding item with quantity less than 1
-    5: adding already existing (purchase list) item
- */
     @Test
     void addingExistingItem() {
         try {
-            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"), 1);
+            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"));
             assertTrue(purchase.getItemsToPurchase()[0].getItemID().equals("apple"),
                     "Wasn't able to correctly add item");
         }
@@ -44,7 +37,7 @@ class PurchaseTest {
     @Test
     void addingNullItem(){
         try{
-            purchase.addItemToPurchase(null,10);
+            purchase.addItemToPurchase(null);
             fail("Exception should have been tossed, due to the attempt of" +
                     "adding a null-pointer...");
         }
@@ -57,8 +50,8 @@ class PurchaseTest {
     @Test
     void addingSameItem(){
         try {
-            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"), 1);
-            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"), 1);
+            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"));
+            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"));
             assertTrue(purchase.getItemsToPurchase()[0].getAmount() == 2,
                     "Amount wasn't updated as intended");
             assertTrue(purchase.getItemsToPurchase().length == 1,
@@ -74,25 +67,15 @@ class PurchaseTest {
     @Test
     void addingItemWithBiggerQuantity(){
         try {
-            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"), 5);
+            PurchaseItemDTO item = ItemIdentifier.scanItem("apple");
+            item.setAmount(5);
+            purchase.addItemToPurchase(item);
             assertTrue(purchase.getItemsToPurchase()[0].getAmount() == 5,
                     "Amount wasn't successfully added");
         }
         catch (Exception e){
             fail("Null exception should not be possible when" +
                     " adding instantiated PurchaseItemDTO");
-        }
-    }
-
-    @Test
-    void addingItemWithInvalidQuantity(){
-        try{
-            purchase.addItemToPurchase(ItemIdentifier.scanItem("apple"), -5);
-            fail("Exception should have been tossed, due to the attempt of" +
-                    "adding an invalid quantity...");
-        }
-        catch (Exception e){
-            assertTrue(e.getMessage().contains("entered"), "Wrong exception was triggered...");
         }
     }
     /*@Test
