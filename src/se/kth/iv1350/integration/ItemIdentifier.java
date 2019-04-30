78bd15d0f9fdc78785ba9d00868726f0b41583ee
Filip <filipgar@kth.se>
2019-04-30 13:53:33 +0200

    Added code to unit test the scanItem process

--- src/se/kth/iv1350/integration/ItemIdentifier.java
+++ src/se/kth/iv1350/integration/ItemIdentifier.java
@@ -5,6 +5,9 @@ package se.kth.iv1350.integration;
  * @author Filip
  */
 public class ItemIdentifier {
+	//only used for testing and simulating database
+	final static int NR_OF_ITEMS = 5;
+	private static PurchaseItemDTO[] database;
 
 	/***
 	 * Finds item based on its id in the inventory database
@@ -12,6 +15,24 @@ public class ItemIdentifier {
 	 * @return Returns the matching item from the database
 	 */
 	public static PurchaseItemDTO scanItem(String itemID) {
+		createItemsForDatabase();
+		return returnItemFromDatabase(itemID);
+	}
+
+	//only used for testing purposes
+	private static void createItemsForDatabase(){
+		database = new PurchaseItemDTO[NR_OF_ITEMS];
+		database[0] = new PurchaseItemDTO(10,2,1,"apple","This is an apple");
+		database[1] = new PurchaseItemDTO(5,1,1,"food","This is food");
+		database[2] = new PurchaseItemDTO(100,30,1,"desk","This is a desk");
+		database[3] = new PurchaseItemDTO(58,5,1,"pasta","This is pasta");
+		database[4] = new PurchaseItemDTO(32,5,1,"bed","This is a bed");
+	}
+
+	private static PurchaseItemDTO returnItemFromDatabase(String itemID){
+		for (int i = 0; i < NR_OF_ITEMS; i++)
+			if (itemID.equals(database[i].getItemID()))
+				return database[i];
 		return null;
 	}
 
---
+++ tests/se/kth/iv1350/controller/ControllerTest.java
@@ -0,0 +1,31 @@
+package se.kth.iv1350.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+    Controller instance;
+    @BeforeEach
+    void setUp() {
+        instance = new Controller();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    void startNewAPurchase() {
+    }
+
+    @Test
+    void scanItem() {
+    }
+
+    @Test
+    void pay() {
+    }
+}
